31f5239cd3a3e7c78a804986034c5d93d98179b2
hhao99 <hhao99@163.com>
2019-08-01 07:19:30 +0800

    add todo model

---
+++ lib/model/todo.dart
@@ -0,0 +1,23 @@
+import 'dart:collection';
+
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+
+class Todo {
+  String task;
+  bool done = false;
+  Todo(this.task,{this.done = false});
+
+}
+
+class TodoModel with ChangeNotifier {
+  List<Todo> _todos = [];
+
+  TodoModel(this._todos);
+
+  UnmodifiableListView<Todo> get todos => this._todos;
+
+  add(Todo todo) {
+    _todos.add(todo);
+  }
+}
