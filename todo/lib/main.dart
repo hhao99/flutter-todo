a648a0603918e82fcda61a5daf10e21bfae272c9
hhao99 <hhao99@gmail.com>
2024-06-06 09:19:35 -0500

    todo v0.2

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter BLOC Demo'),
     );
   }
 }
@@ -69,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Flutter Demo App',
             ),
           ],
         ),
